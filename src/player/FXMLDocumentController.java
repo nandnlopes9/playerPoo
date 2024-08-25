1719c85e4636becd04afec291751a8e8c6bf9b37
Samuel Lopes <samuelopes2003@gmail.com>
2024-08-25 16:33:09 -0300

    Adicionando o projeto no repositório

---
+++ src/player/FXMLDocumentController.java
@@ -0,0 +1,24 @@
+package player;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * @author Samuka
+ */
+public class FXMLDocumentController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+    
+}
